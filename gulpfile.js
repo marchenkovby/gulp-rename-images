875b4fdf85f480dedc29d199a20d789fc361bde3
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-25 14:54:16 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,51 @@
 import gulp from 'gulp';
 import rename from 'gulp-rename';
 import del from 'del';
-//import maxDirIndex from 'npm-max-dir-index';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers'
-//import debug from 'gulp-debug';
 
 const argv = yargs(hideBin(process.argv)).argv
 
 const deleteImages = () => {
-  return del(['build',], {force:true})
+  return del(['build',], {force:true});
 }
 
 const renameImages = () => {
-    var index = argv.index
-    //var index = maxDirIndex('src/images/', '^.*$');
-    //var index = 1
-    console.log('Begin index: ' + index) 
-    return gulp.src('src/images/***/*.{jpg,jpeg,png,gif,wepb}')
-    .pipe(rename(function (path) 
-    {
-        // if (index <= 9) {
-        //     path.basename = '0' + (index++)
-        // } else {
-        //   path.basename = (index++)
-        // }
-        path.basename = (index++)
-        path.dirname = ''
-        return path;
-    }))
-    .pipe(gulp.dest('build'))
+  var indexArticle = argv.article
+  var indexImage = 1
+  var templateBase = '1'
+  var templateImage = 0
+  var templateArticle = 0
+  return gulp.src('src/images/*.{jpg,jpeg,png,gif,wepb}')
+  .pipe(rename(function (path) {
+    // template for image
+    if (indexImage <= 9) {
+      templateImage = '0' + (indexImage++)
+    } else {
+      templateImage = '' + (indexImage)
+    }
+
+    //template for article
+    if (indexArticle <= 9) {
+      templateArticle = '000' + indexArticle
+    }
+    else if (indexArticle > 9 && indexArticle <= 99){
+      templateArticle = '00' + indexArticle
+    }
+    else if (indexArticle > 99 && indexArticle <= 999){
+      templateArticle = '0' + indexArticle
+    }
+    else if (indexArticle > 999 && indexArticle <= 9999){
+      templateArticle = indexArticle
+    }
+    
+    // all templates
+    path.basename = templateBase + templateArticle + templateImage 
+    return path;
+ }))
+  .pipe(gulp.dest('build'))
 }
 
 gulp.task('default', gulp.series(deleteImages, renameImages));
 
-// npm run rename -- -- test=123
+// npm run rename -- --article 123
