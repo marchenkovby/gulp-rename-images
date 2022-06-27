786334a7638c2843460da24bb39335ce655ede8c
Maksim Marchenkov <marchenkovby@gmail.com>
2022-06-27 22:59:26 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,39 +11,62 @@ const deleteImages = () => {
 }
 
 const renameImages = () => {
-  var indexArticle = argv.article
-  var indexImage = 1
+  var indexArticle = argv.article ? argv.article : 0
+  var indexImage = argv.image ? argv.image : 1
   var templateBase = '1'
   var templateImage = 0
   var templateArticle = 0
   return gulp.src('src/images/*.{jpg,jpeg,png,gif,wepb}')
   .pipe(rename(function (path) {
-    // template for image
-    if (indexImage <= 9) {
-      templateImage = '0' + (indexImage++)
-    } else {
-      templateImage = '' + (indexImage)
-    }
+    if (argv.image > 1) {
+      templateBase = '2'
+      if (argv.image >= 1 && argv.image <= 9) {
+        templateImage = '00000' + (argv.image++)
+      } else if (argv.image > 9 && argv.image <= 99) {
+        templateImage = '0000' + (argv.image++)
+      } else if (argv.image > 99 && argv.image <= 999) {
+        templateImage = '000' + (argv.image++)
+      } else if (argv.image > 999 && argv.image <= 9999) {
+        templateImage = '00' + (argv.image++)
+      }  else if (argv.image > 9999 && argv.image <= 99999) {
+        templateImage = '0' + (argv.image++)
+      } else if (argv.image > 99999 && argv.image <= 999999) {
+        templateImage = (argv.image++)
+      } else {
+        templateImage = (argv.image++)
+        templateBase = '3'
+      }
 
-    //template for article
-    if (indexArticle >= 1 && indexArticle <= 9) {
-      templateArticle = '000' + indexArticle
-    }
-    else if (indexArticle > 9 && indexArticle <= 99){
-      templateArticle = '00' + indexArticle
-    }
-    else if (indexArticle > 99 && indexArticle <= 999){
-      templateArticle = '0' + indexArticle
-    }
-    else if (indexArticle > 999 && indexArticle <= 9999){
-      templateArticle = indexArticle
-    }
-    else {
-      templateArticle = '0000'
-    }
+      path.basename = templateBase + templateImage
+      return path;
+    } else {
+      // template for image
+      if (indexImage >= 1 && indexImage <= 9) {
+        templateImage = '0' + (indexImage++)
+      } else {
+        templateImage = '' + (indexImage++)
+      }
+
+      //template for article
+      if (indexArticle >= 1 && indexArticle <= 9) {
+        templateArticle = '000' + indexArticle
+      }
+      else if (indexArticle > 9 && indexArticle <= 99){
+        templateArticle = '00' + indexArticle
+      }
+      else if (indexArticle > 99 && indexArticle <= 999){
+        templateArticle = '0' + indexArticle
+      }
+      else if (indexArticle > 999 && indexArticle <= 9999){
+        templateArticle = indexArticle
+      }
+      else {
+        templateArticle = '0000'
+      }
+  }
     
     // all templates
-    path.basename = templateBase + templateArticle + templateImage 
+    path.basename = templateBase + templateArticle + templateImage
     return path;
  }))
   .pipe(gulp.dest('build'))
